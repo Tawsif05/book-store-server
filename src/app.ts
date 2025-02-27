5a9341520e08d643a82eb90707eb703ea61a5b3a
Tawsif05 <dilafro346@gmail.com>
2025-02-27 17:17:58 +0600

    Update vercel.json file

--- src/app.ts
+++ src/app.ts
@@ -12,11 +12,11 @@ app.use(express.json());
 app.use(cors({origin: ["http://localhost:5173"], credentials: true}));
 app.use("/api/v1", router);
 
-app.use(globalErrorHandler);
-app.use(notFound);
-
 app.get('/', (req: Request, res: Response) => {
   res.send("Hello world");
 });
+app.use(globalErrorHandler);
+app.use(notFound);
+
 
 export default app;
