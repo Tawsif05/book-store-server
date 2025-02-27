00a1760dc68b3f9c455bb6bb62b2c78ce6163fe5
Tawsif05 <dilafro346@gmail.com>
2025-02-27 20:36:20 +0600

    something add

--- dist/app.js
+++ dist/app.js
@@ -12,11 +12,11 @@ const cookie_parser_1 = __importDefault(require("cookie-parser"));
 const app = (0, express_1.default)();
 app.use((0, cookie_parser_1.default)());
 app.use(express_1.default.json());
-app.use((0, cors_1.default)({ origin: ["http://localhost:5173"], credentials: true }));
+app.use((0, cors_1.default)({ origin: ["https://book-store-client-delta.vercel.app"], credentials: true }));
 app.use("/api/v1", routes_1.default);
-app.use(globalErrorHandler_1.globalErrorHandler);
-app.use(notFound_1.default);
 app.get('/', (req, res) => {
     res.send("Hello world");
 });
+app.use(globalErrorHandler_1.globalErrorHandler);
+app.use(notFound_1.default);
 exports.default = app;
--- src/app.ts
+++ src/app.ts
@@ -9,7 +9,7 @@ const app: Application = express();
 
 app.use(cookieParser())
 app.use(express.json());
-app.use(cors({origin: ["http://localhost:5173"], credentials: true}));
+app.use(cors({origin: ["https://book-store-client-delta.vercel.app"], credentials: true}));
 app.use("/api/v1", router);
 
 app.get('/', (req: Request, res: Response) => {
